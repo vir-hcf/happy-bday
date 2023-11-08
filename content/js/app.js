ef187f905cfc2f8f227c0adba25fe3c7f965828d
Dalveer Singh <dalveer.singh@ontariohealth.ca>
2023-11-08 11:36:56 -0500

    Adds new files and begin

---
+++ content/js/app.js
@@ -0,0 +1,34 @@
+$(document).ready(function(){
+    $("#main .birthday-message").hide();
+    $("#main .adventure").hide();
+});
+
+$("#begin").click(function(){
+  $(this).hide();
+  startCountDown();
+});
+function startCountDown() {
+  var countDown = 9;
+  let timeout = setInterval(function() {
+    if(countDown == 9){
+      var audio = new Audio('./content/audio/timer.wav');
+      audio.play();
+    }
+    if(countDown == -1){
+      clearInterval(timeout);
+      $("#main .birthday-message").fadeIn(5000);
+      setTimeout(function(){
+        $("#main .adventure").fadeIn(5000);
+      },5000);
+      $("body").addClass("confetti");
+      $("#main .countdown").hide();
+    }else{
+      $("#main .countdown").html(countDown);
+      countDown--;
+    }
+  },1000);
+}
+
+$("#begin-adventure").click(function(){
+  window.location.href = "./first-adventure.html";
+});
